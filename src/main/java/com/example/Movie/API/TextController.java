0ce991063c9a068009ebbf31eec91ec7707273b7
cesar gutierrez <cesargusa22@gmail.com>
2020-11-08 14:58:30 +0100

    He creado un Controlador llamado TextController con @Controller para que nos devuelva idioma.html y permita cambiar el idioma, luego he creado el idioma.html con thymeleaf para mostrar Peliculas en h1

---
+++ src/main/java/com/example/Movie/API/TextController.java
@@ -0,0 +1,17 @@
+package com.example.Movie.API;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TextController {
+    
+    @GetMapping("/movies/TextLanguage")
+    public String Text(){
+        return "idioma";
+    }
+
+ 
+}
+
